8953a494dbe3c2fe5c5a0e254b4c163f20b428dd
IvanSmiths <info@ivansmiths.com>
2023-03-21 14:16:52 +0100

    add global styling

---
+++ src/components/Parallax.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Parallax() {
+    return (
+        <div className='parallax-wrapper'>
+
+        </div>
+    )
+}
+
+export default Parallax
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;600;800&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
