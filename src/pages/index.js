6a1653426eeacb9bc979606da87650a6aa122d21
IvanSmiths <info@ivansmiths.com>
2023-03-21 14:55:50 +0100

    add about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,15 @@ import React from 'react'
 
 function Home() {
   return (
-    <Parallax />
+    <>
+      <Parallax />
+      <div className='about'>
+        <h2>
+          Lorem ipsum
+        </h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit laborum ipsam corrupti asperiores magnam quos cumque animi tempore vero repellendus, harum odio neque quis, non temporibus. Inventore asperiores repudiandae praesentium ut, fugit quo esse, placeat ullam quibusdam perspiciatis delectus ducimus nihil. Dolorum nam veniam aperiam sapiente corporis! Quisquam, veritatis repellendus?</p>
+      </div>
+    </>
   )
 }
 
