6834d7abbd90ce7946a12156bfd17490a0a8107d
IvanSmiths <info@ivansmiths.com>
2023-03-21 16:34:38 +0100

    add context for avoid animation stop

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -19,97 +19,97 @@ function Parallax() {
     const btn = useRef(null);
 
     useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
-        var tl = gsap.timeline({
-            defaults: { duration: 1 },
-            scrollTrigger: {
-                trigger: parallaxRef.current,
-                start: "top top",
-                end: "5000 bottom",
-                scrub: true,
-                pin: true,
-                onUpdate: (self) => {
-                    setBackground(Math.ceil(self.progress * 100 + 20))
+        let ctx = gsap.context(() => {
+            gsap.registerPlugin(ScrollTrigger);
+            var tl = gsap.timeline({
+                defaults: { duration: 1 },
+                scrollTrigger: {
+                    trigger: parallaxRef.current,
+                    start: "top top",
+                    end: "5000 bottom",
+                    scrub: true,
+                    pin: true,
+                    onUpdate: (self) => {
+                        setBackground(Math.ceil(self.progress * 100 + 20))
+                    },
                 },
-            },
+            });
+            tl.to(
+                mountain3.current,
+                {
+                    y: "-=80",
+                },
+                0
+            );
+            tl.to(
+                mountain2.current,
+                {
+                    y: "-=30",
+                },
+                0
+            );
+            tl.to(
+                mountain1.current,
+                {
+                    y: "+=50",
+                },
+                0
+            );
+            tl.to(
+                stars.current,
+                {
+                    top: 0,
+                },
+                0.5
+            );
+            tl.to(
+                cloudsBottom.current,
+                {
+                    opacity: 0,
+                    duration: 0.5
+                },
+                0
+            );
+            tl.to(
+                cloudsLeft.current,
+                {
+                    x: "-20%",
+                    opacity: 0,
+                },
+                0
+            );
+            tl.to(
+                cloudsRight.current,
+                {
+                    x: "20%",
+                    opacity: 0,
+                },
+                0
+            );
+            tl.to(
+                sun.current,
+                {
+                    y: "+=210",
+                },
+                0
+            );
+            tl.to(
+                copy.current,
+                {
+                    y: "-250%",
+                    opacity: 1
+                },
+                0
+            );
+            tl.to(
+                btn.current,
+                {
+                    opacity: 1,
+                },
+                1.5
+            );
         });
-        tl.to(
-            mountain3.current,
-            {
-                y: "-=80",
-            },
-            0
-        );
-        tl.to(
-            mountain2.current,
-            {
-                y: "-=30",
-            },
-            0
-        );
-        tl.to(
-            mountain1.current,
-            {
-                y: "+=50",
-            },
-            0
-        );
-        tl.to(
-            stars.current,
-            {
-                top: 0,
-            },
-            0.5
-        );
-        tl.to(
-            cloudsBottom.current,
-            {
-                opacity: 0,
-                duration: 0.5
-            },
-            0
-        );
-        tl.to(
-            cloudsLeft.current,
-            {
-                x: "-20%",
-                opacity: 0,
-            },
-            0
-        );
-        tl.to(
-            cloudsRight.current,
-            {
-                x: "20%",
-                opacity: 0,
-            },
-            0
-        );
-        tl.to(
-            sun.current,
-            {
-                y: "+=210",
-            },
-            0
-        );
-        tl.to(
-            copy.current,
-            {
-                y: "-250%",
-                opacity: 1
-            },
-            0
-        );
-        tl.to(
-            btn.current,
-            {
-                opacity: 1,
-            },
-            1.5
-        );
-        return () => {
-            tl.kill();
-        };
+        return () => ctx.revert();
     }, []);
 
     return (
