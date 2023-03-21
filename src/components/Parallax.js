a36e7bb1677ffa6b397e3b3dc658171bd1b83080
IvanSmiths <info@ivansmiths.com>
2023-03-21 14:51:51 +0100

    add all images

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -4,7 +4,16 @@ function Parallax() {
     const [background, setBackgound] = useState(20)
     return (
         <div className='parallax-wrapper'>
-            <div style={{ background: `linear-gradient(#0F2B9C, #673D7D ${background}%, #A74A67, #EDFC54 )` }} className='parallax'></div>
+            <div style={{ background: `linear-gradient(#0F2B9C, #673D7D ${background}%, #A74A67, #EDFC54 )` }} className='parallax'>
+                <img className='mountain-3' src="/parallax/mountain-3.svg" />
+                <img className='mountain-2' src="/parallax/mountain-2.svg" />
+                <img className='mountain-1' src="/parallax/mountain-1.svg" />
+                <img className='sun' src="/parallax/sun.svg" />
+                <img className='clouds-bottom' src="/parallax/cloud-bottom.svg" />
+                <img className='clouds-left' src="/parallax/clouds-left.svg" />
+                <img className='clouds-right' src="/parallax/clouds-right.svg" />
+                <img className='stars' src="/parallax/stars.svg" />
+            </div>
         </div>
     )
 }
